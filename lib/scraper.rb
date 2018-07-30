e691bebe627041a93d50bcb0de599d881c55bf5b
Bihan Viranga <bihanviranga@gmail.com>
2018-07-30 15:03:11 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,19 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = File.read(index_url)
+    page = Nokogiri::HTML(doc)
+    students = []
+
+    page.css("div.student-card").each do |student|
+      hashed_st = {
+        :name => student.css("a div.card-text-container h4.student-name").text
+        :location => student.css("a div.card-text-container p.student-location").text
+        :profile_url => student.css("a").attributes("href").value
+      }
+      students << hashed_st 
+      
+    end 
   end
 
   def self.scrape_profile_page(profile_url)
